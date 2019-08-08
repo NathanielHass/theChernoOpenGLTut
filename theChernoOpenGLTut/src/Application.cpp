c95c2c4025c06daff2e19c18dad45f672a992af6
NathanielHass <hass.nathaniel@gmail.com>
2019-08-08 00:20:30 -0400

    Switch to Modern OpenGL and made vertex buffers

--- theChernoOpenGLTut/src/Application.cpp
+++ theChernoOpenGLTut/src/Application.cpp
@@ -22,24 +22,36 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	/* Check for errors in glewinit before proceeding */
 	if (glewInit() != GLEW_OK)
 		std::cout << "ERROR! GLEW IS NOT OKAY" << std::endl;
 
+	/* output OpenGL Version Number in use */
 	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	/* Load or Create Graphical Data */
+	float imageData[6] =
+	{//		x		y
+		 -0.5f,	-0.5f,	// point 1
+		 -0.0f,	 0.5f,	// point 2
+		  0.5f,	-0.5f,	// point 3
+	};
+
+	/* Format Data for use by OpenGL */
+	unsigned int vertexBuffer;						//create ID tag
+	glGenBuffers(1, &vertexBuffer);					//assign memory to tag
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);	//switch OpenGL state to specified buffer
+
+	/* Prepare Data before entering the loop */
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), imageData, GL_STATIC_DRAW);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
+		glClear(GL_COLOR_BUFFER_BIT);				//reset framebuffer
+		
 		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glBegin(GL_TRIANGLES);
-		
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(-0.0f,  0.5f);
-		glVertex2f( 0.5f, -0.5f);
-		
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -48,6 +60,7 @@ int main(void)
 		glfwPollEvents();
 	}
 
+	/* Shutdown GLFW and exit program */
 	glfwTerminate();
 	return 0;
 }
